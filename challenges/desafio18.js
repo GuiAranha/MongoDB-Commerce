169741e5fd048e70646d31332ff17be5f70b5c53
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-09 15:03:55 -0300

    feat: inclui bancon no final da lista de ingredientes em Big Mac e Quarteirao com Queijo

---
+++ challenges/desafio18.js
@@ -0,0 +1,7 @@
+// inclui bancon no final da lista de ingredientes em Big Mac e Quarteirao com Queijo
+db.produtos.updateMany({ $or: [
+  { nome: { $eq : "Big Mac"}},
+  { nome: { $eq: "Quarteirão com Queijo"}},]}
+,{ $push: { ingredientes: "bacon" }});
+db.produtos.find({}
+,{ _id: 0, nome: 1, ingredientes: 1 });
